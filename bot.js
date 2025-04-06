9f5a169daec8f1198de9d7aaa67e879a1419ec29
ikyrenata <ikidrag1@gmail.com>
2025-04-06 09:47:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,8 @@ const Profile = mongoose.model('Profile', profileSchema);
 // Discord bot client setup
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// Connect to MongoDB (mongodb+srv://ikyrenata:<db_password>@cluster0.hvjvoyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
-mongoose.connect('your_mongodb_uri_here', { useNewUrlParser: true, useUnifiedTopology: true })
+// Connect to MongoDB (mongodb+srv://ikyrenata:Djiky112@cluster0.hvjvoyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+mongoose.connect('mongodb+srv://ikyrenata:Djiky112@cluster0.hvjvoyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err));
 
