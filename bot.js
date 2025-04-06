2beced1490a5253e20c62cdfe643bdc84dd37e50
ikyrenata <ikidrag1@gmail.com>
2025-04-06 09:25:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const Profile = mongoose.model('Profile', profileSchema);
 // Discord bot client setup
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// Connect to MongoDB (you'll need to replace this with your MongoDB URI)
+// Connect to MongoDB (mongodb+srv://ikyrenata:<db_password>@cluster0.hvjvoyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
 mongoose.connect('your_mongodb_uri_here', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err));
